265a9ceffd06b05b4709da81ae66f58eaa4948ed
biscrab <biscrab@naver.com>
2022-04-19 21:34:34 +0900

    👾 :: 버그 수정

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -28,7 +28,6 @@ function MyApp({ Component, pageProps }) {
       <title>쉽게 배우는 일본어</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossoOrigin="anonymous"></link>
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet"></link>
-      <script src="https://kit.fontawesome.com/ed23fb601b.js" crossOrigin="anonymous"></script>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.2.0/css/all.min.css" integrity="sha512-6c4nX2tn5KbzeBJo9Ywpa0Gkt+mzCzJBrE1RB6fmpcsoN+b/w/euwIMuQKNyUoU/nToKN3a8SgNOtPrbW12fug==" crossOrigin="anonymous" />
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
